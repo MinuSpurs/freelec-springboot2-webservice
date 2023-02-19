08106be8660885b056ee42b3fda65cdda3e5ab4d
Minwoo <gapbu123@nate.com>
2023-02-20 01:07:17 +0900

    study

--- src/main/java/com/minwoo/book/springboot/service/posts/PostsService.java
+++ src/main/java/com/minwoo/book/springboot/service/posts/PostsService.java
@@ -1,12 +1,15 @@
 package com.minwoo.book.springboot.service.posts;
 import com.minwoo.book.springboot.domain.posts.Posts;
 import com.minwoo.book.springboot.domain.posts.PostsRepository;
+import com.minwoo.book.springboot.web.dto.PostsListResponseDto;
 import com.minwoo.book.springboot.web.dto.PostsResponseDto;
 import com.minwoo.book.springboot.web.dto.PostsSaveRequestDto;
 import com.minwoo.book.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -26,6 +29,13 @@ public class PostsService {
         posts.update(requestDto.getTitle(), requestDto.getContent());
 
         return id;
+    }
+
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc() {
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new)
+                .collect(Collectors.toList());
     }
 
     public PostsResponseDto findById (Long id){
--- src/main/java/com/minwoo/book/springboot/web/IndexController.java
+++ src/main/java/com/minwoo/book/springboot/web/IndexController.java
@@ -1,11 +1,18 @@
 package com.minwoo.book.springboot.web;
+import com.minwoo.book.springboot.service.posts.PostsService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.ui.Model;
 
+@RequiredArgsConstructor
 @Controller
 public class IndexController {
+
+    private final PostsService postsService;
     @GetMapping("/")
-    public String index() {
+    public String index(Model model){
+        model.addAttribute("posts", postsService.findAllDesc());
         return "index";
     }
 
