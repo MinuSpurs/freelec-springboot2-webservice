5c11bf12e65c3765b369cebaabdaf15437abb9e2
Minwoo <gapbu123@nate.com>
2023-02-22 04:09:24 +0900

    study

--- src/main/java/com/minwoo/book/springboot/web/IndexController.java
+++ src/main/java/com/minwoo/book/springboot/web/IndexController.java
@@ -1,4 +1,5 @@
 package com.minwoo.book.springboot.web;
+import com.minwoo.book.springboot.config.auth.dto.SessionUser;
 import com.minwoo.book.springboot.service.posts.PostsService;
 import com.minwoo.book.springboot.web.dto.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
@@ -7,14 +8,21 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import javax.servlet.http.HttpSession;
+
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
 
     private final PostsService postsService;
+    private final HttpSession httpSession;
     @GetMapping("/")
     public String index(Model model){
         model.addAttribute("posts", postsService.findAllDesc());
+        SessionUser user = (SessionUser) httpSession.getAttribute("user");
+        if (user != null){
+            model.addAttribute("userName", user.getName());
+        }
         return "index";
     }
 
