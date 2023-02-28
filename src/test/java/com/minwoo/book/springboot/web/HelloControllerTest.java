49453e1213411b0ddf67876520ad685bdff44316
Minwoo <gapbu123@nate.com>
2023-03-01 04:03:02 +0900

    study

--- src/test/java/com/minwoo/book/springboot/web/HelloControllerTest.java
+++ src/test/java/com/minwoo/book/springboot/web/HelloControllerTest.java
@@ -1,8 +1,12 @@
 package com.minwoo.book.springboot.web;
+import com.minwoo.book.springboot.config.auth.SecurityConfig;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -13,11 +17,16 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
-@WebMvcTest(controllers = HelloController.class)
+@WebMvcTest(controllers = HelloController.class,
+            excludeFilters = {
+            @ComponentScan.Filter(type= FilterType.ASSIGNABLE_TYPE, classes = SecurityConfig.class)
+            }
+            )
 public class HelloControllerTest {
     @Autowired
     private MockMvc mvc;
 
+    @WithMockUser(roles="USER")
     @Test
     public void return_hello() throws Exception{
         String hello = "hello";
@@ -25,6 +34,7 @@ public class HelloControllerTest {
         mvc.perform(get("/hello")).andExpect(status().isOk()).andExpect(content().string(hello));
     }
 
+    @WithMockUser(roles="USER")
     @Test
     public void return_helloDto() throws Exception{
         String name = "hello";
